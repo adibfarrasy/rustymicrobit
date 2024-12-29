afdb5cfb95f4258138307e82659c60515f060027
Adib Farrasy <adibfarrasy@gmail.com>
2024-12-29 10:17:20 +0700

    using nrf52832-hal

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use nrf52832_pac::Peripherals;
+use embedded_hal::digital::{OutputPin, PinState};
+use hal::pac;
+use nrf52832_hal::{self as hal, gpio::Level};
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let p: Peripherals = Peripherals::take().unwrap();
-    p.P0.pin_cnf[21].write(|w| w.dir().output());
-    p.P0.pin_cnf[28].write(|w| w.dir().output());
+    let p = pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+
+    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
+    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
+
     let mut is_on: bool = false;
     loop {
-        p.P0.out.write(|w| w.pin21().bit(is_on));
-        for _ in 0..400_000 {
+        let _ = row1.set_state(PinState::from(is_on));
+        for _ in 0..100_000 {
             nop();
         }
         is_on = !is_on;
